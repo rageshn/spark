11c5163e25994feb65150943482666a889c39d6d
Ragesh <ragesh.narayanan@gmail.com>
2019-04-28 10:28:18 +0530

    Reads json data

--- kafka/simpleconsumer.py
+++ kafka/simpleconsumer.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql import functions as f
 
 spark = SparkSession.builder.appName("Basic Consumer with Kafka").master("local").getOrCreate()
 
@@ -6,4 +8,13 @@ movies_data = spark.readStream.format("kafka")\
                     .option("kafka.bootstrap.servers", "localhost:9092")\
                     .option("subscribe", "movies-sample").load()
 
-movies_data.writeStream.outputMode("append").format("console").option("truncate", False).start().awaitTermination()
+movies_schema = StructType([StructField("cast", ArrayType(StringType(), True), True),
+                            StructField("year", IntegerType(), True),
+                            StructField("genres", ArrayType(StringType(), True), True),
+                            StructField("title", StringType(), True)])
+
+movies_value = movies_data.selectExpr("CAST(value as string)")
+data = movies_value.select(f.from_json(f.col("value").cast("string"), movies_schema).alias("data")).select("data.*")
+final = data.writeStream.outputMode("append").format("console").start()
+
+final.awaitTermination()
